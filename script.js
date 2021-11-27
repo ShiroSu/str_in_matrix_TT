40b2cead62312b4716e3743f365179e2b13957e7
ShiroSu <61061507+ShiroSu@users.noreply.github.com>
2021-11-27 20:52:06 +0400

    user_str

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const stringy = "QIGNAEKIRLRNGEAE",
-    str1 = "KING",
+    str1 = "GRAEK",
     notan="Not an instance"
 
 
@@ -20,8 +20,8 @@ const matrix_row = inp=> {
 
 const init = (inp, mat)=> {
     let init_cells = []
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat.length; j++) {
             if (inp[0] === mat[i][j])
             init_cells.push({ "first_element": mat[i][j], "i": i, "j": j })
         }
@@ -30,54 +30,84 @@ const init = (inp, mat)=> {
 }
 
 const user_string = (inp, mat)=> {
-    const size = mat[0].length
-    let cells = []
-
-    console.log(inp)
+    let size = mat.length
+    let new_mat = Array(size)
+    let init_cells = init(inp, mat)
     
-    for (let n = 0; n < inp.length; n++) {
-        const str_char = inp[n]
-        if (cells.length > 0) {
-            const ind_i = cells.slice(-1)[0].i
-            const ind_j = cells.slice(-1)[0].j
-            
-            let neighbours = []
+    for (let i = 0; i<4; i++) {
+        let b = Array(4)
+        new_mat[i] = b
+    }
+    console.log(new_mat)
+
+    for (const str_char of inp) {
+        console.log(str_char, ":")
+        for (let i = 0; i < size; i++) {
+            row = new_mat[i]
+            for (let j = 0; j < size; j++) {
+                if (str_char === mat[i][j]) {
+                    new_mat[i][j] = mat[i][j]
+                }
+            }
+        }
+    }
+
+    console.log(init_cells)
+    for (const first_init of init_cells) {
+        let cells = [first_init]
+        for (const str_char of inp) {
             for (let i = 0; i < size; i++) {
-                row = mat[i].slice()
-                neighbours[i] = row
-                for (let j = 0; j < size; j++) {
-                    elem = row[j]
-                    if ((i === ind_i || j === ind_j) && !(i === ind_i && j === ind_j)) {
-                        if ((i >= ((ind_i > 0) ? ind_i-1 : 0)) && (i < ind_i+2)) {
-                            if ((j >= ((ind_j > 0) ? ind_j-1 : 0)) && (j < ind_j+2)) {
-                                // neighbours[i][j] = elem
-                                if (str_char == elem) {
-                                    cells.push({"item": str_char, "i": i, "j": j})
-                                    break
-                                }
-                                // console.log(elem + ": ", '\n\ti: ', i, ind_i, '\n\tj: ', j, ind_j, '\n', neighbours)
-                            }
-                        }
+                if (i > (cells.slice(-1)[0].i)-2 && i < (cells.slice(-1)[0].i)+2) {
+                    // console.log("It's okey!")
+                    const current = new_mat[i].indexOf(str_char)
+                    console.log(str_char, i, current)
+                    if (current > 0 && current > (cells.slice(-1)[0].j)-2 && current < (cells.slice(-1)[0].j)+2) {
+                        if ((i === cells.slice(-1)[0].i || current === cells.slice(-1)[0].j) && !(i === cells.slice(-1)[0].i && current === cells.slice(-1)[0].j))
+                            cells.push({ "element": str_char, "i": i, "j": current })
                     }
                 }
             }
         }
-        else {
-            for (let i = 0; i < size; i++) {
-                row = mat[i].slice()
-                for (let j = 0; j < size; j++) {
-                    mat_char = row[j]
-                    if (str_char == mat_char) {
-                        cells[0] = {"first item": str_char, "i": i, "j": j}
-                        break
-                    }
-                } //break
-            }
-        }
+        // if (cells.length === inp.length) 
+        return cells
     }
-    // if (cells.length === inp.length) return cells
-    // else return notan + "\nOnly " + cells.length
-    return cells
+    return "Nah...  "
+
+    // let cells = []
+
+    // console.log(inp)
+    
+    // for (let n = 0; n < inp.length; n++) {
+    //     const str_char = inp[n]
+    //     if (cells.length > 0) {
+    //         const ind_i = cells.slice(-1)[0].i
+    //         const ind_j = cells.slice(-1)[0].j
+            
+    //         let neighbours = []
+    //         for (let i = 0; i < size; i++) {
+    //             row = mat[i].slice()
+    //             neighbours[i] = row
+    //             for (let j = 0; j < size; j++) {
+    //                 elem = row[j]
+    //                 if ((i === ind_i || j === ind_j) && !(i === ind_i && j === ind_j)) {
+    //                     if ((i >= ((ind_i > 0) ? ind_i-1 : 0)) && (i < ind_i+2)) {
+    //                         if ((j >= ((ind_j > 0) ? ind_j-1 : 0)) && (j < ind_j+2)) {
+    //                             // neighbours[i][j] = elem
+    //                             if (str_char == elem) {
+    //                                 cells.push({"item": str_char, "i": i, "j": j})
+    //                                 break
+    //                             }
+    //                             // console.log(elem + ": ", '\n\ti: ', i, ind_i, '\n\tj: ', j, ind_j, '\n', neighbours)
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // // if (cells.length === inp.length) return cells
+    // // else return notan + "\nOnly " + cells.length
+    // return cells
 }
 
 const str2 = big_str=> {
